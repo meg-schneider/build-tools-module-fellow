caef5baa85b8651fb925af6182bcd232928da9b2
Meg Schneider <megschneider616@gmail.com>
2023-07-29 12:30:09 -0400

    complete readme instructions, no extension

--- server/server.js
+++ server/server.js
@@ -13,15 +13,17 @@ const leaderList = [
 
 app.get('/api/leaders', (req, res) => {
   return res.status(200).send(leaderList);
-});
+}); 
 
-
-// statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '../build')));
-// serve index.html on the route '/'
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-});
+// will only set up these 2 route handlers if we're in production mode
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  });
+}
 
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        exclude: /node_modules/,
+        exclude: /node_modules/, // ignore files in node_modules
         loader: 'babel-loader',
         options: {
           // for babel-loader 8.x | babel 7.x
